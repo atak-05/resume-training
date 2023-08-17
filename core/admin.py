1470e522c29b8ed18e86081b637ea6fce6b949c4
gizem <gizemtuncer17@gmail.com>
2023-08-17 16:36:43 +0300

    refactor: application of general settings

--- core/admin.py
+++ core/admin.py
@@ -8,7 +8,7 @@ from core.models import *
 class GeneralSettingAdmin(admin.ModelAdmin):
     list_display = ['id','name','description','parameters','updated_date','created_date',]
     search_fields = ['name','description','parameters',]
-    list_editable = ['name','description','parameters',]
+    list_editable = ['description','parameters',]
     class Meta:
         model = GeneralSetting
 
