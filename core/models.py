382403de0a3cc6a5d356e3f3e8a6949117b0922c
gizem <gizemtuncer17@gmail.com>
2023-08-21 22:40:32 +0300

    refactor: create abstract model

--- core/models.py
+++ core/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 
 # Create your models here.
-class GeneralSetting(models.Model):
+
+
+class AbstractModel(models.Model):
+    updated_date = models.DateField(auto_now_add=True, blank=True, verbose_name='updated date', help_text='')
+    created_date = models.DateField(auto_now=True, blank=True, verbose_name='created date', help_text='')
+
+    class Meta:
+        abstract = True
+
+class GeneralSetting(AbstractModel):
     name = models.CharField(max_length=254, default='', verbose_name='name', help_text='this is variable of settings')
     description = models.CharField(max_length=254, default='', blank=True, verbose_name='description', help_text='')
     parameters = models.CharField(max_length=254, default='', blank=True, verbose_name='parameters', help_text='')
-    updated_date = models.DateField(auto_now_add=True, blank=True, verbose_name='updated date', help_text='')
-    created_date = models.DateField(auto_now=True, blank=True, verbose_name='created date', help_text='')
+
 
     def __str__(self):
         return f"General Setting: {self.name}"
@@ -18,12 +26,11 @@ class GeneralSetting(models.Model):
         ordering = ("name",)
 
 
-class ImageSettings(models.Model):
+class ImageSettings(AbstractModel):
     name = models.CharField(default='', max_length=254,blank=True, verbose_name='Name', help_text='This is variable of setting.')
     description=models.CharField(default='', max_length=254,blank=True, verbose_name='Description', help_text='')
     file=models.ImageField(default='', blank=True, verbose_name='Image', help_text='',upload_to='images/')
-    updated_date = models.DateField(auto_now_add=True, blank=True, verbose_name='updated date', help_text='')
-    created_date = models.DateField(auto_now=True, blank=True, verbose_name='created date', help_text='')
+
 
     def __str__(self):
         return f"Image Setting: {self.name}"
