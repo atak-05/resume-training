0050f9cfcb3c391893bfc367ab1ee4d3beeb9d1a
gizem <gizemtuncer17@gmail.com>
2023-08-24 17:17:27 +0300

    feat: create message model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from contact.models import Message
+
+
 # Register your models here.
+@admin.register(Message)
+class Message(admin.ModelAdmin):
+    list_display = ['id', 'name','email','subject','message','updated_date','created_date']
+    search_fields = ['name','email','subject',]
+    class Meta:
+        model =Message
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from core.models import AbstractModel
 # Create your models here.
+class Message(AbstractModel):
+    name= models.CharField(max_length=254,default="",blank=True,verbose_name="Name")
+    email= models.EmailField(max_length=254,default="",blank=True,verbose_name="Email")
+    subject= models.CharField(max_length=254,default="",blank=True,verbose_name="Subject")
+    message= models.TextField(default="",blank=True,verbose_name="Message")
+
+    def __str__(self):
+        return f"Message {self.name} is sending to {self.email} about {self.subject} "
+
+    class Meta:
+         verbose_name="Message"
+         verbose_name_plural="Messages"
+         ordering = ('name',)
